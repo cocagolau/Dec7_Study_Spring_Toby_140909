e9c42b8be191ac8bef9444d6aec42c437cdd81f2
cocagolau <cocagolau@gmail.com>
2014-09-12 02:16:22 +0900

    ch2.3

--- practice/tobyspring/src/main/java/me/dec7/user/dao/UserDao.java
+++ practice/tobyspring/src/main/java/me/dec7/user/dao/UserDao.java
@@ -9,78 +9,17 @@ import javax.sql.DataSource;
 
 import me.dec7.user.domain.User;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 
-/*
- * 1.7.4 / 의존관계 주입의 응용
- * 
- * 1. 기능구현의 교환
- *  - DB를 변경할 경우, 기존 DI를 사용하지 않았다면 변경할 곳은 엄청 많을 것임 / 수정 후 오류관리도 필요
- *  - DI를 이용했다면, ConnectionMaker interface를 구현한 새로운 class로만 교체
- *  
- * 2. 부가기능 추가
- *  - 만약 Dao아 DB 연결횟수를 세야할 경우, connection을 연결하는 부분에 코드를 추가, 종료 후 제거하기 보다
- *    container가 사용하는 설정정보만 수정하여 런타임 의존관계만 새롭제 정의
- *    
- *    me.dec7.user.dao.CountingConnectionMaker 참조
- *    me.dec7.user.dao.DaoFactory 참조
- * 
- */
+
 public class UserDao {
-	// DataSource 로 변경
-	// private ConnectionMaker connectionMaker;
 	private DataSource dataSource;
 
-	/*
-	 * 1.7.2 / Runtime 의존관계 설정
-	 * 
-	 * 의존관계
-	 *  - A가 B에 의존하고 있다는 것은 B가 변경될 때 그것이 A에 영향을 미친다는 것
-	 *  - 의존관계는 반드시 방향성이 있음
-	 *  	- B는 A에 의존하지 않음
-	 */
-	
-	/*
-	 * UML에서 말하는 의존관계 / 설계모델 관점
-	 *  - UserDao는 ConnectionMaker interface에 의존
-	 *  	- interface가 변경시 그 영향을 UserDao가 받음
-	 *  	- ConnectionMaker를 구현한 Class를 다른 것으로 바뀌어도 UserDao에는 영향을 주지 않음
-	 *  		- interface에 대해서만 의존관계 형성시 구현 class와 관계가 느슨해지며 변화에 덜 영향받음
-	 *  		- 결합도가 낮은 상태
-	 *  
-	 * Runtime시 Object사이에서 만들어지는 의존관계
-	 *  - 설계시점의 의존관계가 실체화된 것
-	 *  - 모델링 시점의 의존관계와 성격이 다름
-	 *  - 실제 사용대상인 오브젝트 (dependent object, 의존 오브젝트)
-	 *  
-	 * 의존관계 주입
-	 *  - 구체적의 의존 오브젝트, 그것을 사용할 주체를 client가 runtime시 연결해주는 작업
-	 *   1. class, model에는 runtime시점의 의존관계가 드러나 있지 않음 / interface에만 의존
-	 *   2. runtime 시점의 의존관계는 제3의 존재가 결정 (container, factory)
-	 *   3. 의존관계는 사용할 오브젝트에 대한 레퍼런스를 외부에서 제공함으로 이루어짐
-	 *   
-	 * DI설계의 핵심
-	 *  - 설계시점에 몰랐던 두 오브젝트의 관계를 runtime시 도와주는 제3의 존재가 있음.
-	 *    ApplicationContext, BeanFactory, IoC container...
-	 */
-	
-	// 생성자를 통한 DI
-	/*
-	public UserDao(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
-	}
-	*/
-	
-	/*
-	 *  setter method DI 방식을 적용
-	 *  DaoFactory도 함께 수정해야함
-	 */
 	public void setDataSource(DataSource dataSource) {
-//		this.connectionMaker = connectionMaker;
 		this.dataSource = dataSource;
 	}
 
 	public void add(User user) throws SQLException, ClassNotFoundException {
-		// Connection c = this.connectionMaker.makeConnection();
 		Connection c = this.dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
@@ -95,26 +34,64 @@ public class UserDao {
 	}
 
 	public User get(String id) throws SQLException, ClassNotFoundException {
-		// Connection c = this.connectionMaker.makeConnection();
 		Connection c = this.dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
 		ResultSet rs = ps.executeQuery();
-		rs.next();
 		
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+		// user를 null로 초기화
+		User user = null;
+		
+		// 결과 존재시 데이터를 담음
+		if (rs.next()) {
+			user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));			
+		}
+		
 		
 		rs.close();
 		ps.close();
 		c.close();
 		
+		// user결과가 null인 경우 exception을 던짐
+		if (user == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		
 		return user;
 	}
+	
+	public void deleteAll() throws SQLException, ClassNotFoundException {
+		Connection c = this.dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public int getCount() throws SQLException, ClassNotFoundException {
+		Connection c = this.dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		
+		int count = rs.getInt(1);
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return count;
+	}
+	
 }
 
 
--- practice/tobyspring/src/test/java/me/dec7/user/dao/UserDaoTest.java
+++ practice/tobyspring/src/test/java/me/dec7/user/dao/UserDaoTest.java
@@ -7,74 +7,228 @@ import java.sql.SQLException;
 
 import me.dec7.user.domain.User;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 /*
- * UserDaoTest 문제점
- *  - 테스트 진행과정이 모두 수동
- *  	- 결과를 눈으로 확인
+ * P183, JUnit 테스트 수행 방식
+ * 
+ * Framework는 스스로 주도권을 가지고 주도적으로 동작
+ * 개발자가 만든 코드는 프레임워크에 의해 수동적으로 실행
+ * 그러므로 사용되는 코드만으로 실행흐름이 보이지 않으므로 프레임워크가 어떻게 사용할지 잘 알아야함
+ * 
+ * 
+ * 1. 테스트 클래스에서 @Test가 붙은 public, void, parameter가 없는 테스트 메소드를 모두 찾음 
+ * 2. 테스트 클래스의 오브젝트를 하나 만든다,
+ * 3. @Before가 붙은 메소드가 있으면 실행
+ * 4. @Test가 붙은 메소드를 하나 호출하고 테스트 결과를 저장
+ * 5. @After가 붙은 메소드가 있으면 실행
+ * 6. 나머지 테스트 메소드에 대해서 2~5번 반속
+ * 7. 모드 테스트결과를 종합해서 전달
+ * 
+ * 중요
+ *  - 테스트 메소드를 실행할 때마다 테스트 클래스의 오브젝트를 새로 만듦
+ *  - 테스트 클래스가 @Test 테스트 메소드를 두개 가지고 있다면
+ *    테스트 실행중 JUnit은 클래스의 오브젝트를 두 번 만듦
+ *    
+ * 왜? 테스트마다 오브젝트를 만드는가?
+ *  - 각 테스트가 서로 영향을 주지 않고 독립적으로 실행됨을 확실히 보장하기 위함
+ *  - 만약 테스트 메소드 일부에서 공통적으로 사용되는 코드가 있는 경우
+ *  	- @Before를 사용하기 보다
+ *  	  일반적인 메소드 추출 방법을 사용하여 메소드 분리 후 테스트 메소드에서 직접 호출하도록 함
  *  
- *  - 실행의 번거로움
- *  	- 테스트당 다른 main()을 보유, dao가 늘어나면 main()도 늘어남
+ *  
+ * 픽스처
+ *  - 테스트를 수행하는 데 필요한 정보나 오브젝트
+ *  - 반복적으로 생성되므로 일반적으로 @Before에 생성해두면 편리
+ */
+
+
+/*
+ * 2.3.1, JUnit 테스트 실행방법
+ * 
+ *  1. JUnitCore를 사용해 콘솔에 결과를 확인
+ *   - 가장 간단하지만 테스트 수가 많아지면 관리 어려움 
+ *   
+ *  2. 보통 자바 IDE에 내장된 JUnit 테스트 지원도구 사용
+ */
+
+
+/*
+ * 포괄적인 테스트
+ * 개발자는 항상 긍정적인 테스트 코드만 작성하는 경향이 있음
+ * 
+ * "항상 네거티브 테스트를 먼저 만들라" 스프링 창시자 로드 존슨
+ *  - ex) get() method에서 id가 존재하지 않는 경우
+ */
+
+/*
+ * 기능설계를 위한 테스트
+ *  - 추가하고 싶은 기능을 먼저 코드로 표현 --> 테스트 코드
+ *  - getUserFailure()
+ *  	- 조건: 어떤 조건을 가지고 / 가져올 사용자 정보가 존재하지 않는 경우
+ *  	- 행위: 무엇을 할 때	/ 존재하지 않는 id로 get()을 실행하면
+ *  	- 결과: 어떤 결과가 나옴	/ 특별한 예외가 던져짐
+ * 
+ * 테스트 주도 개발
+ *  - 테스트를 만들어가며 개발하는 방법이 주는 장점을 극대화한 개발 프로세스
+ *  - "실패한 테스트를 성공시키기 위한 목적이 아닌 코드는 만들지 않는다" 
  */
 public class UserDaoTest {
 	
 	/*
-	 * 개선2, 실행의 번거로움
+	 * 2.3.5, 테스트 코드 개선
 	 * 
-	 * JUnit 사용
+	 * 반복코드 별도의 메소드로 추출
+	 */
+	private UserDao dao;
+	private User user1;
+	private User user2;
+	private User user3;
+	
+	// @Test(매번 테스트) 메소드를 실행하기 전에 먼저 실행돼야 하는 메소드를 정의
+	@Before
+	public void setUp() {
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:/applicationContext.xml");
+		
+		// 픽스쳐 생성
+		dao = context.getBean("userDao", UserDao.class);
+		
+		user1 = new User("dec1", "동규1", "111");
+		user2 = new User("dec2", "동규2", "222");
+		user3 = new User("dec3", "동규3", "333");
+	}
+	
+	
+	/*
+	 * Test결과의 일관성
 	 * 
-	 * 프레임워크의 기본원리가 IoC임.
-	 * 따라서 test를 main()으로 한다는 것은 제어권을 직접 갖는다는 의미로 프레임워크에 적합하지 않음
-	 * 
-	 * main() method --> 일반 method
+	 * 매번 UserDaoTest 테스트를 실행하기 전 DB의 User 데이터를 모두 삭제해줘야 했음
+	 *  - 테스트가 외부 상태에 따라 결과가 달라짐 
+	 *  - 코드에 변경사항이 없다면 테스트는 항상 동일해야 함
+	 *  
+	 * UserDaoTest 문제
+	 *  - 이전 테스트 때문에 DB에 등록된 중복 데이터가 있을 수 있다는 점
+	 *  - 가장 좋은 해결책은 테스트 후 테스트 정보를 삭제해서 이전 상태를 만들어 주는 것
+	 *  	- deleteAll(), getCount() 추가
 	 */
 	
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("classpath:/applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+// 중복으로 추출
+//		ApplicationContext context = new GenericXmlApplicationContext("classpath:/applicationContext.xml");
+//		UserDao dao = context.getBean("userDao", UserDao.class);
 		
-		User user = new User();
-		user.setId("dec7");
-		user.setName("동규");
-		user.setPassword("127");
+		// db 초기화
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		// 픽스쳐 @Before에 모음
+		// User user1 = new User("dec1", "동규1", "111");
+		// User user2 = new User("dec2", "동규2", "222");
+		// user.setId("dec7");
+		// user.setName("동규");
+		// user.setPassword("127");
 		
 		// add()
-		dao.add(user);
+		dao.add(user1);
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
 		
 		// get()
-		User user2 = dao.get(user.getId());
+		User userget1 = dao.get(user1.getId());
+		assertThat(userget1.getName(), is(user1.getName()));
+		assertThat(userget1.getPassword(), is(user1.getPassword()));
 		
-		// test결과
-		/*
-		if (!user.getName().equals(user2.getName())) {
-			System.out.println("테스트 실패 (name)");
-			
-		} else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("테스트 실패 (password)");
-			
-		} else {
-			System.out.println("조회 테스트 성공");
-			
-		}
-		*/
-		assertThat(user2.getName(), is(user.getName()));
-		assertThat(user2.getPassword(), is(user.getPassword()));
+		User userget2 = dao.get(user2.getId());
+		assertThat(userget2.getName(), is(user2.getName()));
+		assertThat(userget2.getPassword(), is(user2.getPassword()));
+		
 	}
 	
+	/*
+	 *  get() 예외 조건에 대한 테스트
+	 *  
+	 *  get()시 전달된 id에 해당하는 값이 없는 경우
+	 *   1. null과 같은 특별한 값을 반환
+	 *   2. 정보를 찾을 수 없다는 예외를 던짐
+	 *   
+	 *  보통 테스트 중 예외가 발생하면 테스트 메소드는 중단되고 테스트는 실패
+	 *  하지만 이 경우, 특정 예외가 발생하면 테스트가 성공, 그렇지 않으면 실패로 간주 해야함
+	 *  
+	 *  예외는 비교할 수 있는 성질이 아니므로 assertThat() 메소드로 검증 불가
+	 *  JUnit은 예외조건 테스트를 위한 새로운 방법을 제공함 
+	 */
+	
+	/*
+	 *  테스트 중 발생할 것으로 기대되는 예외 클래스를 지정함
+	 * 
+	 *  이 경우 보통의 테스트와 반대로
+	 *  정상적으로 테스트가 마치면 테스트가 실패하고
+	 *  expected에 지정한 예외가 발생하면 테스트간 성공함
+	 */
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getUserFailure() throws SQLException, ClassNotFoundException {
+//		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		/*
+		 * 예외 발생지점
+		 * get() method에서 EmptyResultDataAccessException 예외가 발생하지 않아 테스트 실패
+		 * 
+		 * UserDao.get() method 수정
+		 */
+		dao.get("unknown_id");
+	}
+	
+	
+	/*
+	 * addAndGet(), count() test는 어떤 순서로 실행될지 모름
+	 *  - JUnit은 특정한 테스트 메소드의 실행순서를 보장하지 않음
+	 *  - 테스트 결과가 테스트 실행순서에 영향을 받는다면 테스트를 잘못 만든 것
+	 * 
+	 */
+	@Test
+	public void count() throws SQLException, ClassNotFoundException {
+//		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+//		User user1 = new User("dec1", "서", "ddeecc11");
+//		User user2 = new User("dec2", "동", "ddeecc22");
+//		User user3 = new User("dec3", "규", "ddeecc33");
+		
+		dao.deleteAll();
+		assertThat(dao.getCount(), is(0));
+		
+		dao.add(user1);
+		assertThat(dao.getCount(), is(1));
+		
+		dao.add(user2);
+		assertThat(dao.getCount(), is(2));
+		
+		dao.add(user3);
+		assertThat(dao.getCount(), is(3));
+		
+		
+	}
 	
 	/*
 	 * JUnit 테스트 실행
 	 * 
 	 * JUnit도 spring container처럼 어디선가 한번은 JUnit framework를 실행해야됨
-	 */
+	 * 
+	 *  --> Java IDE가 제공하는 기능을 사용하자 
+	 *  	
 	public static void main(String[] args) {
 		JUnitCore.main("me.dec7.user.dao.UserDaoTest");
 	}
+	
+	*/
 }
 
 
